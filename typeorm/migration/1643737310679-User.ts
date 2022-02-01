aec691ef6d9a6065f236df6ff997bc0f9c5715de
Myltiane Alves <myltiane.aux@gmail.com>
2022-02-01 19:08:10 -0300

    Criando relacionamentos entre tabelas

--- typeorm/migration/1643737310679-User.ts
+++ typeorm/migration/1643737310679-User.ts
@@ -34,12 +34,52 @@ export class User1643737310679 implements MigrationInterface {
                 type: 'datetime',
                 default: 'CURRENT_TIMESTAMP'
             }]
+        }));
+
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [{
+                name: 'id',
+                type: 'int',
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: 'increment',
+            }, {
+                name: 'email',
+                type: 'varchar',
+                length: '250',
+                isNullable: false,
+                isUnique: true,
+            }, {
+                name: 'password',
+                type: 'varchar',
+                length: '255',
+                isNullable: false,
+            }, {
+                name: 'photo',
+                type: 'varchar',
+                length: '255',
+                isNullable: true
+            }, {
+                name: 'personId',
+                type: 'detetime',
+                isNullable: false,
+            }, {
+                name: 'createdAt',
+                type: 'datetime',
+                default: 'CURRENT_TIMESTAMP',
+            }, {
+                name: 'updatedAt',
+                type: 'datetime',
+                default: 'CURRENT_TIMESTAMP',
+            }]
         }))
 
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.dropTable("persons")
+        await queryRunner.dropTable("users")
     }
 
 }
