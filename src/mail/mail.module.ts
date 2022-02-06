ad147b9a7fdc6eae717a37bc1937f5acce4151bf
Myltiane Alves <myltiane.aux@gmail.com>
2022-02-06 13:15:51 -0300

    Adicionado o adapter para criação de template para email

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,25 +1,39 @@
 import { MailService } from './mail.service';
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { resolve } from 'path/posix';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
     imports: [
         MailerModule.forRootAsync({
             useFactory: async () => ({
                 transport: {
-                    host: 'smtp.gmail.com',
-                    port: 587,
+                    host: process.env.MAIL_HOST,
+                    port: Number(process.env.MAIL_PORT),
                     secure: false,
                     auth: {
-                        user: '',
-                        pass: '',
-                    }
+                        user: process.env.MAIL_USER,
+                        pass: process.env.MAIL_PASS,
+                    },
                 },
+                defaults:{
+                    from: `HCODE lab <${process.env.MAIL_FROM}>`
+                },
+                template: {
+                    dir: resolve(__dirname, 'tempalte'),
+                    adapter: new PugAdapter(),  
+                    options: {
+                        strict: true,
+                    }
+                }
             }),
         }),
     ],
     controllers: [],
     providers: [
-        MailService,],
+        MailService,
+    ],
+    exports: [MailService],
 })
 export class MailModule { }
