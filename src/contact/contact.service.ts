c04da5bf935348871c6484fab0336a32081353cf
Myltiane Alves <myltiane.aux@gmail.com>
2022-02-21 22:46:07 -0300

    Finalizando o tabela contact

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -3,15 +3,18 @@ https://docs.nestjs.com/providers#services
 */
 
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ContactService {
 
+    constructor(private db: PrismaService) {}
     
     async create(
         {name, email, message}: 
         {name: string; email:string; message: string;}
     ) {
 
+        
     }
  }
